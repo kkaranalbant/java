bffa2c7afba38c77b3bc25d37dfac5f7a9048614
Kaan Karanalbant <141653270+kkaranalbant@users.noreply.github.com>
2024-01-07 16:35:26 +0300

    Add files via upload

---
+++ SchoolManagementMaven/src/main/java/com/kaan/schoolmanagementmaven/admin/IAdminAddingManager.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.kaan.schoolmanagementmaven.admin;
+
+import com.kaan.schoolmanagementmaven.exception.InvalidPhoneCountryCodeException;
+import com.kaan.schoolmanagementmaven.exception.InvalidPhoneNumberLengthException;
+import com.kaan.schoolmanagementmaven.exception.NotUniqueNameAndLastnameException;
+import java.sql.SQLException;
+import java.util.Map;
+
+/**
+ *
+ * @author kaan
+ */
+public interface IAdminAddingManager {
+
+    Map<String, String> addNormalStudent(String name, String lastName , String phoneNumber) throws SQLException, NotUniqueNameAndLastnameException , InvalidPhoneCountryCodeException , InvalidPhoneNumberLengthException;
+
+    Map<String, String> addWorkingStudent(String name, String lastName,String phoneNumber) throws SQLException, NotUniqueNameAndLastnameException, InvalidPhoneCountryCodeException , InvalidPhoneNumberLengthException;
+
+    Map<String, String> addTeacher(String name, String lastName, String branchName , int salary,String phoneNumber) throws SQLException, NotUniqueNameAndLastnameException, InvalidPhoneCountryCodeException , InvalidPhoneNumberLengthException;
+}
---
+++ SchoolManagementMaven/src/main/java/com/kaan/schoolmanagementmaven/admin/IAdminSettingManager.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.kaan.schoolmanagementmaven.admin;
+
+import com.kaan.schoolmanagementmaven.exception.InvalidBalanceException;
+import java.sql.SQLException;
+import com.kaan.schoolmanagementmaven.exception.InvalidDebtException;
+import com.kaan.schoolmanagementmaven.exception.InvalidLessonCreditException;
+import com.kaan.schoolmanagementmaven.exception.InvalidPassLengthException;
+import com.kaan.schoolmanagementmaven.exception.InvalidPhoneCountryCodeException;
+import com.kaan.schoolmanagementmaven.exception.InvalidPhoneNumberLengthException;
+import com.kaan.schoolmanagementmaven.exception.InvalidSalaryException;
+import com.kaan.schoolmanagementmaven.exception.InvalidUsernameLengthException;
+import com.kaan.schoolmanagementmaven.exception.NotUniqueNameAndLastnameException;
+import com.kaan.schoolmanagementmaven.exception.NotUniqueUsernameAndPassException;
+
+/**
+ *
+ * @author kaan
+ */
+public interface IAdminSettingManager {
+
+    public void setBalance(int uid, int value) throws SQLException, InvalidBalanceException;
+
+    public void setUsername(int uid, String newUsername) throws SQLException, NotUniqueUsernameAndPassException, InvalidUsernameLengthException;
+
+    public void setPass(int uid, String newPass) throws SQLException, NotUniqueUsernameAndPassException, InvalidPassLengthException;
+
+    public void setName(int uid, String newName) throws SQLException, NotUniqueNameAndLastnameException;
+
+    public void setLastName(int uid, String newLastname) throws SQLException, NotUniqueNameAndLastnameException;
+
+    public void setDebt(int uid, int value) throws SQLException, InvalidDebtException;
+
+    public void setLessonCredit(int uid, int value) throws SQLException, InvalidLessonCreditException;
+
+    public void setSalary(int uid, int value) throws SQLException, InvalidSalaryException;
+
+    public void setBranch(int uid, int branchId) throws SQLException;
+
+    public void setPhoneNumber(int uid, String newPhoneNumber) throws SQLException, InvalidPhoneCountryCodeException, InvalidPhoneNumberLengthException ;
+}
